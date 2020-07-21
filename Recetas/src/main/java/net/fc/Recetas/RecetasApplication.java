1c1548a5110dadb91d4c89c5dc53e746216012b5
castilloKlever <68032011+castilloKlever@users.noreply.github.com>
2020-07-21 00:36:22 -0500

    Se añadió los recursos necesario para el desarrollo de la página web

---
+++ Recetas/src/main/java/net/fc/Recetas/RecetasApplication.java
@@ -0,0 +1,13 @@
+package net.fc.Recetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetasApplication.class, args);
+	}
+
+}
